646e54d7219227d5196e5a31afdef39879a104b4
0dr4d3kdb <danielbeta97@gmail.com>
2026-07-06 20:56:52 -0400

    cambios de manifiestos y tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,7 @@ variable "project_name" {
 variable "owner_name" {
   description = "Owner name applied to resources through default provider tags."
   type        = string
+  default     = "Estudiante"
 }
 
 variable "aws_region" {
@@ -32,7 +33,7 @@ variable "vpc_cidr" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS cluster."
   type        = string
-  default     = "1.31"
+  default     = "1.35"
 }
 
 variable "node_or_fargate" {
